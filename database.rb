9782e07b35dc4e4e28c8b9ecd63864bc1590c1d6
Danny <dtinsley000@nyc.yearup.org>
2019-01-02 15:01:02 -0500

    shuffled

--- database.rb
+++ database.rb
@@ -1,18 +1,5 @@
 # Based on http://www.jonathanleighton.com/articles/2011/awesome-active-record-bug-reports/
 
-# Run this script with `bundle exec ruby app.rb`
-require 'sqlite3'
-require 'active_record'
-
-#require classes
-require './models/user.rb'
-require './models/post.rb'
-
-
-
-# Use `binding.pry` anywhere in this script for easy debugging
-require 'pry'
-
 
 # Connect to a sqlite3 database
 # If you feel like you need to reset it, simply delete the file sqlite makes
@@ -27,7 +14,18 @@ ActiveRecord::Base.establish_connection(
 )
 end
 
+# Run this script with `bundle exec ruby app.rb`
+require 'sqlite3'
+require 'active_record'
 
+#require classes
+require './models/user.rb'
+require './models/post.rb'
+
+
+
+# Use `binding.pry` anywhere in this script for easy debugging
+require 'pry'
 
 
 binding.pry
